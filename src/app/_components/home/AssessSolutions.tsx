7295432c85e6dcbdeec4387113ecd24cb56e041a
Bhada Yun <bhadayun@gmail.com>
2024-06-23 18:37:58 -0700

    overflow fix

--- src/app/_components/home/AssessSolutions.tsx
+++ src/app/_components/home/AssessSolutions.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Container, Text, List } from '@mantine/core';
 
 interface Solution {
@@ -10,24 +10,16 @@ interface AssessSolutionsProps {
     solutions: Solution[];
 }
 
-// test
-
 export default function AssessSolutions({ solutions }: AssessSolutionsProps) {
-    const [processedSolutions, setProcessedSolutions] = useState<Solution[]>([]);
-
-    useEffect(() => {
-        setProcessedSolutions(solutions);
-    }, [solutions]);
-
     return (
         <Container mt={40} w={"90%"}>
             <Text style={{ fontSize: 30 }} ta="center" mb="xl">
                 Potential Solutions
             </Text>
 
-            {processedSolutions.length > 0 ? (
+            {solutions.length > 0 ? (
                 <List type="ordered" size="lg" spacing="sm">
-                    {processedSolutions.map((solution, index) => (
+                    {solutions.map((solution, index) => (
                         <List.Item style={{ fontSize: 20.4 }} key={index}>
                             <Text style={{ fontSize: 20.4 }}>{solution.title}</Text>
                             <Text mt="sm">{solution.detail}</Text>
